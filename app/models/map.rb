91a4223c1e3ac8c3f0c8a7c153264e45e0be6ae9
Carly <cfujiyoshi@gmail.com>
2017-06-21 20:25:53 -0700

    Maps routes, more gems.

--- app/models/map.rb
+++ app/models/map.rb
@@ -3,4 +3,8 @@ class Map < ApplicationRecord
 
   has_many :pins
   has_many :favorites, through: :users
+
+  # geocoded_by :address
+  # after_validation :geocode
+  # https://anadea.info/blog/how-to-integrate-google-maps-into-ruby-on-rails-app
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   
-
-
   resources :maps
+
+#     maps GET    /maps(.:format)          maps#index
+#          POST   /maps(.:format)          maps#create
+#  new_map GET    /maps/new(.:format)      maps#new
+# edit_map GET    /maps/:id/edit(.:format) maps#edit
+#      map GET    /maps/:id(.:format)      maps#show
+#          PATCH  /maps/:id(.:format)      maps#update
+#          PUT    /maps/:id(.:format)      maps#update
+#          DELETE /maps/:id(.:format)      maps#destroy
 
 end
