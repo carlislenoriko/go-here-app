6c4a3bc4a6131cab37a8c231d1ca06f8272c324b
Carly <cfujiyoshi@gmail.com>
2017-06-28 20:52:50 -0700

    More work on routes/views.

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,34 +1,45 @@
 class MapsController < ApplicationController
 
   def index
-    # if current_user.id
-    #   user_id = params[:id]
-    @maps = Map.all
-    # else
-    #   # flash[:warning] = "Please log in to see this page!"
-    #   redirect_to "/login"
-    render "index.html.erb"
+    if current_user
+      user = User.find(current_user.id)
+      @maps = user.maps
+      puts @maps
+    else
+      # flash[:warning] = "Please log in to see this page!"
+      redirect_to "/login"
+    end
   end
 
   def new
-
   end
 
   def create
     map = Map.create(
       map_name: params[:map_name],
-      description: params[:description]
+      description: params[:description],
+      user_id: current_user.id
       )
     redirect_to map
   end
 
   def edit
+    @map = Map.find(params[:id])
+    render "edit.html.erb"
   end
 
   def update
+    map = Map.find(params[:id])
+    map.update(
+      map_name: params[:map_name],
+      description: params[:description]
+      )
+    # flash[:info] = "Contact Info Updated"
+    redirect_to "/maps/#{map.id}"
   end
 
   def show
+    @map = Map.find(params[:id])
     render "show.html.erb"
   end
 
