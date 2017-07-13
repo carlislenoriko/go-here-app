4f464c0186ce421371e13a71816ba491b89260d2
Carly <cfujiyoshi@gmail.com>
2017-07-12 19:58:20 -0700

    Staging for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   get '/' => 'users#new'
-  # get '/home' => 'users#show'
+  get '/about' => 'users#about'
 
   resources :users, except: :index
 #     users POST   /users(.:format)          users#create
